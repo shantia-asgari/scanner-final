9637eae13d93317b2ea64c1c9ad7c37db38d9724
shantia-asgari <shntiaasgariiii@gmail.com>
2026-02-02 04:43:50 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // نام مخزن (Repo) شما دقیقاً باید اینجا باشد
-  // اگر نام پروژه شما receipt-scanner-pro است، خط زیر درست است:
   base: "/receipt-scanner-pro1/",
+  build: {
+    // این تنظیمات حیاتی است: هر بار بیلد جدید، اسم فایل‌ها را عوض می‌کند
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-${Date.now()}.[ext]`
+      }
+    }
+  }
 })
