bd32ce84a57851408aa74478907f82753baa783e
shantia-asgari <shntiaasgariiii@gmail.com>
2026-02-12 01:59:15 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scanner-final/",
+  base: "/scanner-final/", // آدرس دقیق مخزن جدید
   build: {
-    // این تنظیمات حیاتی است: هر بار بیلد جدید، اسم فایل‌ها را عوض می‌کند
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-${Date.now()}.js`,
-        chunkFileNames: `assets/[name]-${Date.now()}.js`,
-        assetFileNames: `assets/[name]-${Date.now()}.[ext]`
-      }
-    }
+    outDir: 'dist',
   }
 })
