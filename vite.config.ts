c63b80adb688cd318e2d174608796f2e805a6969
shantia-asgari <shntiaasgariiii@gmail.com>
2026-02-14 14:09:26 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scanner-final/", // آدرس دقیق مخزن جدید
+  base: "/", // این یعنی پروژه در ریشه دامنه اجرا می‌شود 
   build: {
     outDir: 'dist',
   }
